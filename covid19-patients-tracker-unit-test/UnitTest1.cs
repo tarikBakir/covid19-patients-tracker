811085d8c88af01ed2d196497385071931dd07ea
Tarik Bakir <Bakirtarik76@gmail.com>
2022-02-16 17:18:07 -0500

    MSTEST

---
+++ covid19-patients-tracker-unit-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace covid19_patients_tracker_unit_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
